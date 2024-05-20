c22fb2b4fcdf923732d8f9193c8ddcb7dbb341fb
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-05-20 23:33:15 +0500

    Добавьте файлы проекта.

---
+++ ДЗ. Поликлиника/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ДЗ.Поликлиника
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int queueNumber;
+            int waitingTimeInQueue;
+            int appointmentTimeForOnePerson = 10;
+            int hour = 60;
+            int waitingHour;
+            int waitingMinute;
+            Console.WriteLine("Добро пожаловать в нашу поликлинику. Возьмите пожалуйста талон на приём к врачю.");
+            Console.Write("Укажите ваш номер в очереди по талону:");
+            queueNumber = Convert.ToInt32(Console.ReadLine());
+            waitingTimeInQueue=(queueNumber - 1)*appointmentTimeForOnePerson;
+            waitingHour=waitingTimeInQueue/hour;
+            waitingMinute = waitingTimeInQueue % hour;
+            Console.WriteLine($"Ваше время ожидание в очереди: {waitingHour} ч., {waitingMinute} мин.");
+        }
+    }
+}
