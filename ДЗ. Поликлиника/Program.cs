20f5aa600c6d3672371261cf9e8697c1cff2f0be
Kirs4n <165835810+Kirs4n@users.noreply.github.com>
2024-05-21 21:54:28 +0500

    Add files via upload

--- ДЗ. Поликлиника/Program.cs
+++ ДЗ. Поликлиника/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Policy;
 namespace ДЗ.Поликлиника
 {
     internal class Program
@@ -7,16 +8,16 @@ namespace ДЗ.Поликлиника
         {
             int queueNumber;
             int waitingTimeInQueue;
-            int appointmentTimeForOnePerson = 10;
-            int hour = 60;
+            int receptionTimeForOnePersonInMinutes = 10;
+            int minutesInAHour = 60;
             int waitingHour;
             int waitingMinute;
             Console.WriteLine("Добро пожаловать в нашу поликлинику. Возьмите пожалуйста талон на приём к врачю.");
             Console.Write("Укажите ваш номер в очереди по талону:");
             queueNumber = Convert.ToInt32(Console.ReadLine());
-            waitingTimeInQueue=(queueNumber - 1)*appointmentTimeForOnePerson;
-            waitingHour=waitingTimeInQueue/hour;
-            waitingMinute = waitingTimeInQueue % hour;
+            waitingTimeInQueue=(queueNumber - 1)* receptionTimeForOnePersonInMinutes;
+            waitingHour=waitingTimeInQueue/ minutesInAHour;
+            waitingMinute = waitingTimeInQueue % minutesInAHour;
             Console.WriteLine($"Ваше время ожидание в очереди: {waitingHour} ч., {waitingMinute} мин.");
         }
     }
